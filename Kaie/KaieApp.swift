f0776b2b2156f6816aacdb0d8b782ab3237d0b3a
Chris Hyorok Lee <hl3838@columbia.edu>
2025-12-27 22:00:14 -0500

    Initial Commit

---
+++ Kaie/KaieApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaieApp.swift
+//  Kaie
+//
+//  Created by 이효록 on 12/27/25.
+//
+
+import SwiftUI
+
+@main
+struct KaieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KaieTests/KaieTests.swift
@@ -0,0 +1,17 @@
+//
+//  KaieTests.swift
+//  KaieTests
+//
+//  Created by 이효록 on 12/27/25.
+//
+
+import Testing
+@testable import Kaie
+
+struct KaieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
